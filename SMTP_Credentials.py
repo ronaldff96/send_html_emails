0e35a518c1d25ffdd13fcc5084ae9967ce8da1b3
Ronald Fonseca <mail@ronaldff.com>
2021-12-09 12:54:02 -0600

    SMTP_Credentials file added

---
+++ SMTP_Credentials.py
@@ -0,0 +1,9 @@
+#To test the script, please change the profile_name and modifie the dictionary values with your SMTP credentials
+
+profile_name = {
+	'name'     : 'Sender Name',
+	'host'     : 'smtp.hostdomain.com',
+	'port'     : 587,
+	'email'    : 'mail@example.com',
+	'password' : 'P@ssw0rd!',
+}
