de255ee6da68baf38f8fd2bcc1ac6a725c5d364b
Ronald Fonseca <mail@ronaldff.com>
2021-12-09 23:43:08 -0600

    Logger added to script

--- send_email.py
+++ send_email.py
@@ -1,4 +1,14 @@
 #! /usr/bin/python3
+# =============================================================================
+# Created  By  : Ronald Fonseca
+# Creation Date: December 5, 2021
+# =============================================================================
+"""
+This Script allows the user to send many HTML emails using SMTP Connection.
+"""
+# =============================================================================
+# Imports
+# =============================================================================
 import sys
 import smtplib
 import SMTP_Credentials as creds
@@ -65,12 +75,20 @@ def start_smtp(credentials):
 	  }
 	'''
 	#Start Connection
-	print(f'Starting SMTP connection...\n{line}')
-	smtp_connection = smtplib.SMTP(credentials['host'], credentials['port'])
-	smtp_connection.ehlo()
-	smtp_connection.starttls()
-	smtp_connection.login(credentials['email'], credentials['password'])
-	print(f'Successfully connected!\n{line}')
+	try:
+		print(f'Starting SMTP connection...\n{line}')
+		smtp_connection = smtplib.SMTP(credentials['host'], credentials['port'])
+		smtp_connection.ehlo()
+		smtp_connection.starttls()
+		smtp_connection.login(credentials['email'], credentials['password'])
+	except Exception as error:
+		logger.error(f"Error connecting to {credentials['email']}: {error}")
+		except_logger.exception(f"Error connecting to {credentials['email']}: {error}")
+		print(f"Error connecting to {credentials['email']}:")
+		exit()
+	else:
+		print(f'Successfully connected!\n{line}')
+		logger.info(f"Successfully connected to {credentials['email']}")
 	return smtp_connection
 
 def format_message(from_name, from_email, to_email, subject, text, html, reply_to = None):
@@ -124,26 +142,40 @@ def send_email(smtp_connection, from_name, from_email, user_input):
 					html       = HTML CODE OF THE EMAIL
 			        And, Optionally, an extra reply_to argument can be sent, which would be the Reply-To email of the message.
 	'''
-	message_details = (from_name, from_email) + user_input
-	message = format_message(*message_details)
-	print(f'Sending email...\n{line}')
-	smtp_connection.sendmail(from_email, user_input[0], message.as_string())
-	print(f'Message sent to: {user_input[0]}\n{line}')
+	try:
+		message_details = (from_name, from_email) + user_input
+		message = format_message(*message_details)
+		print(f'Sending email...\n{line}')
+		smtp_connection.sendmail(from_email, user_input[0], message.as_string())
+	except Exception as error:
+		logger.error(f"Error sending message to {user_input[0]}: {error}")
+		except_logger.exception(f"Error sending message to {user_input[0]}: {error}")
+		print(f"Error sending message to {user_input[0]}:")
+		exit()
+	else:
+		print(f'Message sent to {user_input[0]}\n{line}')
+		logger.info(f'Message sent to {user_input[0]}')
 
 def main(credentials, messages_list):
 	#Start SMTP Connection
 	smtp_connection = start_smtp(credentials)
 
-	#Get Name and Email
-	from_name  = credentials['name']
-	from_email = credentials['email']
+	try:
+		#Get Name and Email
+		from_name  = credentials['name']
+		from_email = credentials['email']
 
-	#Send messages
-	for user_input in messages_list:
-		send_email(smtp_connection, from_name, from_email, user_input)
+		#Send messages
+		for user_input in messages_list:
+			send_email(smtp_connection, from_name, from_email, user_input)
 
-	#End SMTP Connection	
-	smtp_connection.quit()
+		#End SMTP Connection	
+		smtp_connection.quit()
+	except Exception as error:
+		logger.error(f"Unexpected error: {error}")
+		except_logger.exception(f"Unexpected error: {error}")
+		print(f"Unexpected error ({error}), please check Logs for more info.")
+		exit()
 
 line = '-'*50
 logger, except_logger = start_logger()
@@ -155,8 +187,6 @@ To run this script, please send the following arguments:
 	1) The SMTP Profile name (as stored in the SMTP Credentials File).
 	2) A list (sent as a string) with the user's input.
 		Ex: "[(to_email, subject, text, html, reply_to), ('ex@mple.com', 'test', 'Super cool message', 'Nice HTML Code')]
-
-You can learn more using the help() command.
 				"""	
 	credentials_list = [profile for profile in dir(creds)[8:] if profile != 'test'] 
 	profile = sys.argv[0].lower()
