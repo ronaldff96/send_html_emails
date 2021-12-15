3e8c68f41cc2b1b2d6d97dba26fad2bdb9e54932
Ronald Fonseca <mail@ronaldff.com>
2021-12-15 16:41:13 -0600

    send_email.py improved to receive file paths

--- send_email.py
+++ send_email.py
@@ -91,7 +91,7 @@ def start_smtp(credentials):
 		logger.info(f"Successfully connected to {credentials['email']}")
 	return smtp_connection
 
-def format_message(from_name, from_email, to_email, subject, text, html, reply_to = None):
+def format_message(html_path, from_name, from_email, to_email, subject, text, html, reply_to = None):
 	'''
 	Formats the email content.
 
@@ -109,6 +109,10 @@ def format_message(from_name, from_email, to_email, subject, text, html, reply_t
 	'''
 	print(f'formatting Message for {to_email}...\n{line}')
 	# Create message container - the correct MIME type is multipart/alternative.
+	if html_path:
+		with open(html, 'r', encoding='utf-8') as f:
+			html_code = f.read()
+		html = html_code
 	msg = MIMEMultipart("alternative")
 	msg["From"] = f'{from_name} <{from_email}>'
 	msg["To"] = to_email
@@ -127,7 +131,7 @@ def format_message(from_name, from_email, to_email, subject, text, html, reply_t
 	msg.attach(part2)
 	return msg
 
-def send_email(smtp_connection, from_name, from_email, user_input):
+def send_email(smtp_connection, from_name, from_email, user_input, html_path):
 	'''
 	Sends an email using a SMTP Connection.
 	Requiers the following arguments:
@@ -139,12 +143,13 @@ def send_email(smtp_connection, from_name, from_email, user_input):
 					to_email   = RECEPIENT EMAIL
 					subject    = EMAIL SUBJECT
 					text       = PLAIN TEXT OF THE EMAIL
-					html       = HTML CODE OF THE EMAIL
+					html       = HTML PATH / HTML CODE OF THE EMAIL
 			        And, Optionally, an extra reply_to argument can be sent, which would be the Reply-To email of the message.
+	html_path = BOOLEAN, TRUE IF HTML CONTENT IS SENT AS PATH OR FALSE IF IT IS SENT AS CODE.
 	'''
 	try:
 		message_details = (from_name, from_email) + user_input
-		message = format_message(*message_details)
+		message = format_message(html_path, *message_details)
 		print(f'Sending email...\n{line}')
 		smtp_connection.sendmail(from_email, user_input[0], message.as_string())
 	except Exception as error:
@@ -156,10 +161,9 @@ def send_email(smtp_connection, from_name, from_email, user_input):
 		print(f'Message sent to {user_input[0]}\n{line}')
 		logger.info(f'Message sent to {user_input[0]}')
 
-def main(credentials, messages_list):
+def main(credentials, messages_list, html_path = True):
 	#Start SMTP Connection
 	smtp_connection = start_smtp(credentials)
-
 	try:
 		#Get Name and Email
 		from_name  = credentials['name']
@@ -167,7 +171,7 @@ def main(credentials, messages_list):
 
 		#Send messages
 		for user_input in messages_list:
-			send_email(smtp_connection, from_name, from_email, user_input)
+			send_email(smtp_connection, from_name, from_email, user_input, html_path)
 
 		#End SMTP Connection	
 		smtp_connection.quit()
@@ -186,7 +190,7 @@ To run this script, please send the following arguments:
 
 	1) The SMTP Profile name (as stored in the SMTP Credentials File).
 	2) A list (sent as a string) with the user's input.
-		Ex: "[(to_email, subject, text, html, reply_to), ('ex@mple.com', 'test', 'Super cool message', 'Nice HTML Code')]
+		Ex: "[(to_email, subject, text, html, reply_to), ('ex@mple.com', 'test', 'Super cool message', 'HTML File Path')]
 				"""	
 	credentials_list = [profile for profile in dir(creds)[8:] if profile != 'test'] 
 	profile = sys.argv[0].lower()
@@ -195,7 +199,6 @@ To run this script, please send the following arguments:
 		print(error_message)
 	else:
 		profile = sys.argv[1].lower()
-		print(profile)
 		if profile in credentials_list:
 			exec(f'credentials = creds.{profile}')
 		else:
